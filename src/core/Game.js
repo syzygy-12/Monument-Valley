2986af32efe103f96330a81f653370013c5de3c9
Syzygy12 <liu-s23@mails.tsinghua.edu.cn>
2024-12-13 19:34:40 +0800

    解决github路径问题2

--- src/core/Game.js
+++ src/core/Game.js
@@ -44,7 +44,9 @@ export default class Game {
     // 动态加载 LevelSelectBox.js
     if (!this.levelSelectBoxLoaded) {
       this.levelSelectScript = document.createElement("script");
-      this.levelSelectScript.src = `${window.location.origin}/src/utils/LevelSelectBox.js`;
+      const basePath = window.location.pathname.replace(/\/[^/]*$/, '/');
+      console.log(basePath);
+      this.levelSelectScript.src = `${basePath}src/utils/LevelSelectBox.js`;
       this.levelSelectScript.onload = () => {
         //console.log("LevelSelectBox.js 已加载");
       }
